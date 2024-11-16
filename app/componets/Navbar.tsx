596567af8148b02f86e19ae6a382ba5ebaedd980
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-16 14:44:12 -0500

    created sing in and sign out buttons/ little logic

--- app/componets/Navbar.tsx
+++ app/componets/Navbar.tsx
@@ -1,14 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
+import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
+import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <header className='p-10 bg-blue-950 text-white'>
-    <nav className='flex justify-betwee items-center'>
-        <Link href="/"> <img src='/logo.png' className="rounded-full" width={100}/></Link>
-    </nav>
-    </header>
-  )
-}
+    <header className="p-10 bg-blue-950 text-white">
+      <nav className="flex justify-betwee items-center">
+        <Link href="/">
+          <img src="/logo.png" className="rounded-full" width={100} />
+        </Link>
+        <div className="flex text-right gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span>Sign Out</span>
+              </button>
 
-export default Navbar
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <button onClick={signIn("github")}>
+              <span>Login</span>
+            </button>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
