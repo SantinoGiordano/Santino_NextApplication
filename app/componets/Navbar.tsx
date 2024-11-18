bf2bd0359143af31a11c7b08c57c6794b89ff2bc
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-18 10:38:11 -0500

    added "responsive" text theme

--- app/componets/Navbar.tsx
+++ app/componets/Navbar.tsx
@@ -6,26 +6,26 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="p-10 bg-blue-950 text-white">
-      <nav className="flex justify-betwee items-center">
+    <header className="p-10 text-2xl bg-dbNav text-white">
+      <nav className="flex justify-between items-center">
         <Link href="/">
           <img src="/logo.png" className="rounded-full" width={100} />
         </Link>
-        <div className="flex text-right gap-5">
+        <div className="flex gap-5 pl-5">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="font-sans text-responsiveHeader transition duration-300 hover:scale-125 hover:shadow-lg pr-3">Create</span>
               </Link>
               <form action={async () => {
                   "use server";
                   await signOut();
                 }}
               >
-                <button type="submit">Sign Out</button>
+                <button type="submit" className="font-sans text-responsiveHeader hover:shadow-lg pr-3">Sign Out</button>
               </form>
 
-              <Link href={`/user/${session?.id}`}>
+              <Link href={`/user/${session?.id}`} className=" font-sans text-responsiveHeader hover:shadow-lg">
                 <span>{session?.user?.name}</span>
               </Link>
             </>
@@ -36,7 +36,9 @@ const Navbar = async () => {
                 await signIn("github");
               }}
             >
-              <button type="submit">Login through Github</button>
+              <button type="submit" className=" font-sans text-responsiveHeader transition duration-300 hover:scale-125 hover:shadow-lg">
+                Log In
+              </button>
             </form>
           )}
         </div>
