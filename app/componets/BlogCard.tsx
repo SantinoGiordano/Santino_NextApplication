522fa3383a0fbb06b8ff256c61c8890caef8f12d
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-20 16:09:24 -0500

    cleaned up code spacing and formated document

--- app/componets/BlogCard.tsx
+++ app/componets/BlogCard.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"; // Ensure SSR-compatible Link
 
 const BlogCard = ({ blogs }: { blogs: BlogTypeCard }) => {
   return (
-    <li className="startup-card group">
+    <li className="startup-card group ">
       {/* Container for Date and Views */}
       <div className="flex-between">
         <p className="startup_card_date">{formatDate(blogs._createdAt)}</p>
@@ -23,19 +23,44 @@ const BlogCard = ({ blogs }: { blogs: BlogTypeCard }) => {
         ) : (
           <p className="text-16-medium line-clamp-1">Anonymous</p>
         )}
+
         <Link href={`/blogs/${blogs._id}`}>
-        <h3 className="text-26-semibold line-clamp-1">{blogs.title}</h3>
+          <h3 className="text-26-semibold line-clamp-1">{blogs.title}</h3>
         </Link>
+
         <Link href={`/user/${blogs.author._id}`}>
-          <img src="https://placehold.co/600x600" alt="placeholder" width={48} height={48} className="rounded-full"/> 
+          <img
+            src="https://placehold.co/600x600"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
           {/* if theres issues with placwholdergo 2:08:5 */}
         </Link>
+
         <Link href={`/blogs/${blogs._id}`}>
-        <p className="startup-card_decs mt-5 mb-5">
-          {blogs.description}
-        </p>
-          <img src={blogs.image} alt="placeholder"className="blog-card_img"></img>
+          <p className="startup-card_decs mt-5 mb-5">{blogs.description}</p>
+          <img
+            src={blogs.image}
+            alt="placeholder"
+            className="blog-card_img"
+          ></img>
         </Link>
+
+        <div className="flex-between gap-3 mt-5">
+          <Link href={`/?query=${blogs.category?.toLowerCase() ?? ""}`}>
+            <p className="text-16-medium">
+              {blogs.category ?? "Uncategorized"}
+            </p>
+          </Link>
+
+          <button className="startup-card_btn">
+            <Link href={`/blogs/${blogs._id}`}>
+              Details
+            </Link>
+          </button>
+        </div>
       </div>
     </li>
   );
